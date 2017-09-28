af84b11a5508d56894e3439ad631995356f8ed25
Krisjanis Rijnieks <krisjanis.rijnieks@gmail.com>
2017-09-28 10:46:41 +0300

    Fix changing spaces into dots

--- chext.sh
+++ chext.sh
@@ -1,29 +1,31 @@
 #!/bin/bash
 
-DIR="files"
-EXT="m4v"
+DIR="./files"
+EXT="txt"
 CHEXT="mp4"
 
 cd "./$DIR"
 
 for i in * ; do
-	
-	for j in $(echo $i | tr "." "\n"); do
-    	if [ -z "$BASENAME" ]; then
-    		BASENAME="$PREV"
-    	else
-    		BASENAME="$BASENAME.$PREV"
-    	fi 
-    	PREV="$j"
-    done
+    EXTLEN=$(echo "$EXT" | awk '{ print length($0); }')
+    echo "EXTLEN $EXTLEN"
 
-    if [ "$PREV" == "$EXT" ]; then
-    	mv "$i" "$BASENAME.$CHEXT"
-    	echo "Renamed $i to $BASENAME.$CHEXT"
+    EXTENSION=$(echo "$i" | rev | cut -c 1-"$EXTLEN" | rev)
+    echo "EXTENSION $EXTENSION"
+
+    BASENAME=$(echo "$i" | rev | cut -c 4- | rev)
+    echo "BASENAME $BASENAME"
+
+    if [ "$EXTENSION" == "$EXT" ]; then
+        mv "$i" "$BASENAME$CHEXT"
+        echo "Renamed $i to $BASENAME$CHEXT"
+    else
+        echo "Extension does not match; skipping this file"
     fi
     
+    unset EXTLEN
+    unset EXTENSION
     unset BASENAME
-    unset PREV
 done
 
 cd ..
